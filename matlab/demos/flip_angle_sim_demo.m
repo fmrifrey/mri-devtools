4975b3dcd0b69198b648ee88c26645623b938ad0
fmrifrey <djfrey@umich.edu>
2023-07-12 16:12:32 -0400

    Added better visualization of flipping to the flip angle sim demo

--- matlab/demos/flip_angle_sim_demo.m
+++ matlab/demos/flip_angle_sim_demo.m
@@ -1,15 +1,21 @@
-%% Inputs and such
 f_mean = 1/20; % mean precession frequency (kHz)
-f_std = f_mean / 4; % std of precession frequency
-N = 4; % number of spins in population
+N = 2; % number of spins in population
+if (N == 1)
+    f_std = 0; % std of precession frequency (kHz)
+else
+    f_std = f_mean / 10; % std of precession frequency (kHz)
+end
 TE = 10; % echo time (ms)
-t = 0:1:50; % timing (ms)
+t_end = 200; % ms
+t = 0:0.5:t_end; % timing (ms)
 
 % create flip angle schedule
 flips = zeros(length(t),2);
 flips(t == TE/2,1) = 90; % add a tipdown at time 5
-for t_tmp = TE:TE:(50 - mod(50,TE)) % at times 10, 20, 30, 40
-    flips(t == t_tmp,2) = 180; % add a refocuser
+tmp = 1;
+for t_tmp = TE:TE:(t_end - mod(t_end,TE)) % at times 10, 20, 30, 40
+    flips(t == t_tmp,2) = tmp*120; % add a refocuser
+    tmp = tmp*-1;
 end
 
 %% Run sim
@@ -53,6 +59,7 @@ for i = 1:length(t)
 end
 
 %% Plot results
+path = [];
 for i = 1:length(t)
 
     subplot(4,2,1:4)
@@ -60,18 +67,30 @@ for i = 1:length(t)
     % loop through spins
     for n = 1:N
         % plot the magnetization
-        plot3([0,M(1,i,n)], [0,M(2,i,n)], [0,M(3,i,n)], 'Linewidth', 1);
+        plot3([0,0.75*M(1,i,n)], [0,0.75*M(2,i,n)], [0,0.75*M(3,i,n)], 'r', 'Linewidth', 0.5);
         hold on
     end
     
     % plot the net mag and axis
-    M_net = mean(M(:,i,:),3);
-    plot3([-1,1],[0,0],[0,0],'--k');
-    plot3([0,M_net(1)], [0,M_net(2)], [0,M_net(3)], 'Linewidth', 3);
+    M_net = mean(M(:,:,:),3);
+    plot3([0,M_net(1,i)], [0,M_net(2,i)], [0,M_net(3,i)], 'r', 'Linewidth', 3);
+    
+    % plot the path
+    if any(abs(flips(i,:)) > 0, 2)
+        for j = 0:20
+            path = [path, rmat3D('xy',flips(i,:)/180*pi*j/20)*inv(rmat3D('xy',flips(i,:)/180*pi))*M_net(:,i)];
+        end
+    else
+        path = [path, M_net(:,i)];
+    end
+    plot3(path(1,:),path(2,:),path(3,:),'--','Color',[0.5,0.5,0.5]);
+    
+    % plot the axises
+    plot3([-1,1],[0,0],[0,0],'-k');
     text(-1.1,0,0,'-x'); text(1.1,0,0,'x');
-    plot3([0,0],[-1,1],[0,0],'--k');
+    plot3([0,0],[-1,1],[0,0],'-k');
     text(0,-1.1,0,'-y'); text(0,1.1,0,'y');
-    plot3([0,0],[0,0],[-1,1],'--k');
+    plot3([0,0],[0,0],[-1,1],'-k');
     text(0,0,-1.1,'-z'); text(0,0,1.1,'z');
     zlim([-1,1])
     hold off
@@ -88,8 +107,8 @@ for i = 1:length(t)
     
     % plot the net magnetization
     subplot(4,2,7:8)
-    plot(t(1:i),mean(M(1,1:i,:),3)); hold on
-    plot(t(1:i),mean(M(2,1:i,:),3)); hold off
+    plot(t(1:i),M_net(1,1:i,:)); hold on
+    plot(t(1:i),M_net(2,1:i,:)); hold off
     xlim([0,t(end)]); ylim([-1,1]);
     title('net magnetization')
     legend('x','y');
